dce6efcdbadd649b044b25d548bfeead6346268e
Jim29800 <jbouvier1173@gmail.com>
2017-08-02 15:12:57 +0200

    ajout des cases a cliquer

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+//Creation du tableau
 var col = 3;
 var ligne = 3;
 var nbLigne = "";
 var nbCol = "";
-for (var i = 0; i <col; i++) {
-    nbCol += "<div class='col-sm-2' id='" + i + "'></div>"
+for (var i = 0; i < col; i++) {
+    nbCol += "<div class='col-sm-2 case' data-colonne='" + i + "'></div>"
 }
 
-for (var i = 0; i <ligne; i++) {
-    nbLigne += "<div class='row' id='"+ i + "'>" + nbCol + "</div>"
+for (var i = 0; i < ligne; i++) {
+    nbLigne += "<div class='row' data-ligne='" + i + "'>" + nbCol + "</div>"
 }
-
 $(".container").html(nbLigne)
+//case a cliquer
+var compteur = 0
+$(".case").click(function () {
+    // var choix2 = $(this).data("colonne");
+    // var choix1 = $(this).parent().data("ligne");
+    // console.log("Ligne : " + choix1 + " colone : " + choix2)
+    if ($(this).text() === "" && compteur%2 === 0){
+        console.log("vide")
+        $(this).text("X")
+        compteur ++
+    }
+    else if ($(this).text() === "" && compteur%2 === 1){
+        console.log("vide")
+        $(this).text("O")
+        compteur ++
+    }
+    else{
+        console.log("plein")
+    }
+})
 
-
-
-// <div class="row">
-//             <div class="col-sm-2">
-
-//             </div>
