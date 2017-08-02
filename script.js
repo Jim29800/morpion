d01c9948ec4e17856b3af95f4b38b2ac1a1b557f
Jim29800 <jbouvier1173@gmail.com>
2017-08-02 15:29:12 +0200

    recuperation dans un tableau de la case

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var col = 3;
 var ligne = 3;
 var nbLigne = "";
 var nbCol = "";
+var tableauCroix = []
+var tableauRond = []
+var compteur = 0
 for (var i = 0; i < col; i++) {
     nbCol += "<div class='col-sm-2 case' data-colonne='" + i + "'></div>"
 }
@@ -12,23 +15,32 @@ for (var i = 0; i < ligne; i++) {
 }
 $(".container").html(nbLigne)
 //case a cliquer
-var compteur = 0
 $(".case").click(function () {
-    // var choix2 = $(this).data("colonne");
-    // var choix1 = $(this).parent().data("ligne");
-    // console.log("Ligne : " + choix1 + " colone : " + choix2)
-    if ($(this).text() === "" && compteur%2 === 0){
+    var choix2 = $(this).data("colonne");
+    var choix1 = $(this).parent().data("ligne");
+    if ($(this).text() === "" && compteur % 2 === 0) {
         console.log("vide")
         $(this).text("X")
-        compteur ++
+        compteur++
+        tableauCroix.push("" + choix1 + choix2);
+        console.log(tableauCroix)
     }
-    else if ($(this).text() === "" && compteur%2 === 1){
+    else if ($(this).text() === "" && compteur % 2 === 1) {
         console.log("vide")
         $(this).text("O")
-        compteur ++
+        compteur++
+        tableauRond.push("" + choix1 + choix2);
+        console.log(tableauRond)
     }
-    else{
+    else {
         console.log("plein")
     }
 })
+//condition de victoire
+
+
+// ligne/colonne gagnante :
+//ligne :       00 01 02 / 10 11 12 / 20 21 22
+//colonne :     00 10 20 / 01 11 21 / 02 12 22
+//diagonalle :  00 11 22 / 02 11 20
 
